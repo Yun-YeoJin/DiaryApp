8a97fd4182f2e77753db8bd105daf84fb122a050
YunYeoJin <duwls0349@naver.com>
2022-08-21 15:37:05 +0900

    Initial Commit

---
+++ DiaryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiaryApp
+//
+//  Created by 윤여진 on 2022/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
