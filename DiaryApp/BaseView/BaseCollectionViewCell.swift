269e906c66c417df218db5cc35b7d4e062f7942b
YunYeoJin <duwls0349@naver.com>
2022-08-21 15:48:46 +0900

    Feat) gitignore & BaseView, BaseVC, BaseCollectionViewCell 생성

---
+++ DiaryApp/BaseView/BaseCollectionViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  BaseCollectionViewCell.swift
+//  DiaryApp
+//
+//  Created by 윤여진 on 2022/08/21.
+//
+
+import UIKit
+
+class BaseCollectionViewCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configureUI()
+        setConstraints()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureUI() { }
+    
+    func setConstraints() { }
+}
